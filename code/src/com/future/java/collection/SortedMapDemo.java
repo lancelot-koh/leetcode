5f436c1958385d752ec0017b0ef3b4c3a5df4335
Yong Gao <yong.gao@sap.com>
2021-04-11 20:41:35 +0800

    add demo

---
+++ code/src/com/future/java/collection/SortedMapDemo.java
@@ -0,0 +1,17 @@
+package com.future.java.collection;
+
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class SortedMapDemo {
+    /**
+     * SortedMap Implementation
+     * TreeMap
+     *
+     */
+
+    public static void main(String[] args) {
+        SortedMap sortedMap = new TreeMap();
+
+    }
+}
