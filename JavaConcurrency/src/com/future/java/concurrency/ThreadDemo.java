b4b65287ab77c176ad02e45a5c7478e29df50c23
Yong Gao <yong.gao@sap.com>
2021-04-14 09:41:00 +0800

    add concurrency package

---
+++ JavaConcurrency/src/com/future/java/concurrency/ThreadDemo.java
@@ -0,0 +1,8 @@
+package com.future.java.concurrency;
+
+public class ThreadDemo {
+    public static void main(String[] args) {
+        Thread thread = new Thread();
+        thread.start();
+    }
+}
